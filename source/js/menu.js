32347a4028be752bd3fb10d10780cd099c5b9ef2
Max <39831143+doshchicyn@users.noreply.github.com>
2019-07-29 01:40:52 +0300

    верстка мобильных страниц + css + интерактивное меню (js)

---
+++ source/js/menu.js
@@ -0,0 +1,13 @@
+var navMain = document.querySelector('.main-menu');
+var navToggle = document.querySelector('.main-menu__toggle-btn');
+
+navMain.classList.remove('main-menu--nojs');
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-menu--closed')) {
+    navMain.classList.remove('main-menu--closed');
+    navMain.classList.add('main-menu--opened');
+  } else {
+    navMain.classList.add('main-menu--closed');
+    navMain.classList.remove('main-menu--opened');
+  }
+});
